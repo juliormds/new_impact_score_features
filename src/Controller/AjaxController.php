07ae5cf70eacf2af0b34ff22af41e0556a0282e0
juliormds <julio@rmdslab.com>
2020-11-30 11:21:49 -0800

    made changes

--- src/Controller/AjaxController.php
+++ src/Controller/AjaxController.php
@@ -27,29 +27,38 @@ class AjaxController extends ControllerBase{
 
     $result = $query->fetchAll();
 
-    $current_time = time();
+    /** Check if the user is in the database **/
 
-    $date1 = date('Y-m-d', $result[0]->created); /** Convert created timestamp to year, month, and day format **/
+    if(count($result) > 0){
 
-    $created_time = strtotime($date1);
+      $current_time = time();
 
-    $datediff1 = $current_time - $created_time;
+      $date1 = date('Y-m-d', $result[0]->created); /** Convert created timestamp to year, month, and day format **/
 
-    $number_of_days_active = round($datediff1 / (60 * 60 * 24 * 180) );
+      $created_time = strtotime($date1);
+
+      $datediff1 = $current_time - $created_time;
+
+      $number_of_days_active = round($datediff1 / (60 * 60 * 24 * 180) );
 
 
-    /** Number of days since last login **/
+      /** Number of days since last login **/
 
-    $date2 = date('Y-m-d', $result[0]->login); /** Convert login timestamp to year, month, and day format **/
+      $date2 = date('Y-m-d', $result[0]->login); /** Convert login timestamp to year, month, and day format **/
 
-    $login_time = strtotime($date2);
+      $login_time = strtotime($date2);
 
-    $datediff2 = $current_time - $login_time;
+      $datediff2 = $current_time - $login_time;
 
-    $number_of_days_since_last_login = round($datediff2 / (60 * 60 * 24) );
+      $number_of_days_since_last_login = round($datediff2 / (60 * 60 * 24) );
 
       //echo "Number of active days " . $number_of_days_active . "<br />";
       //echo "Number of days since last login " . $number_of_days_since_last_login . "<br />";
+
+    } else {
+      $number_of_days_active = 0;
+      $number_of_days_since_last_login = 0;
+    }
 
 
       /** Query for grabbing Number of days since last upload **/
@@ -81,19 +90,27 @@ class AjaxController extends ControllerBase{
 
     /** Query for grabbing Number of recent viewing others (180 days) **/
 
-    $query3 = $db->query('SELECT * FROM {visitors} WHERE visitors_id = :id', [':id' => $uid]);
+    $query3 = $db->query('SELECT * FROM {visitors} WHERE visitors_uid = :id', [':id' => $uid]);
 
     $result3 = $query3->fetchAll();
 
-    $date4 = date('Y-m-d', $result3[0]->visitors_date_time);
+    /** Check if user is in the database **/
 
-    $begin_time = strtotime($date4);
+    if(count($result3) > 0){
 
-    $datediff4 = $current_time - $begin_time;
+      $date4 = date('Y-m-d', $result3[0]->visitors_date_time);
 
-    $num_days_recent_viewing_others = round($datediff4 / (60 * 60 * 24 * 180) );
+      $begin_time = strtotime($date4);
 
-    //echo "Number of recent viewing others " . $num_days_recent_viewing_others ;
+      $datediff4 = $current_time - $begin_time;
+
+      $num_days_recent_viewing_others = round($datediff4 / (60 * 60 * 24 * 180) );
+
+      //echo "Number of recent viewing others " . $num_days_recent_viewing_others ;
+
+    } else {
+      $num_days_recent_viewing_others = 0;
+    }
 
     /** Update to the impact score details table **/
 
